0d4c5dae4bb7de18343083e65c2093af22cbb534
Ahmad Awwad <aawwad172@gmail.com>
2024-08-29 11:34:14 +0300

    link

--- presentaiton/WorkFlows/LeaveRequestApprovalWorkflow.cs
+++ presentaiton/WorkFlows/LeaveRequestApprovalWorkflow.cs
@@ -6,6 +6,8 @@ using Elsa.Services.Workflows;
 using Elsa.Activities;
 using Elsa.Expressions;
 using Elsa.Events;
+using Elsa.Services.Models;
+using System;
 
 
 namespace Presentation.WorkFlows;
@@ -15,26 +17,38 @@ public class LeaveRequestApprovalWorkflow : IWorkflow
     public void Build(IWorkflowBuilder builder)
     {
         builder
-            .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "Leave Request Submitted."))
-            .Then<WriteLine>(activity => activity.Set(x => x.Text, "Sending request to Manager for approval."))
-            .Then<IfElse>(ifElse => ifElse
-                .Condition(context => GetManagerApproval())
-                .When(OutcomeNames.True)
-                    .Then<WriteLine>(activity => activity.Set(x => x.Text, "Manager approved the leave request."))
-                    .Then<IfElse>(innerIfElse => innerIfElse
-                        .Condition(context => GetHRApproval())
-                        .When(OutcomeNames.True)
-                            .Then<WriteLine>(activity => activity.Set(x => x.Text, "HR approved the leave request."))
-                            .Then<WriteLine>(activity => activity.Set(x => x.Text, "Leave request is granted."))
-                            .Then(activity => NotifyEmployee("Leave request approved"))
-                            .Then(activity => NotifyHR("Leave request approved"))
-                        .When(OutcomeNames.False)
-                            .Then<WriteLine>(activity => activity.Set(x => x.Text, "HR rejected the leave request."))
-                            .Then(activity => NotifyEmployee("Leave request rejected"))
-                    )
-                .When(OutcomeNames.False)
-                    .Then<WriteLine>(activity => activity.Set(x => x.Text, "Manager rejected the leave request."))
-                    .Then(activity => NotifyEmployee("Leave request rejected"))
-            );
+               .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "Leave Request Submitted."))
+               .Then<WriteLine>(activity => activity.Set(x => x.Text, "Sending request to Manager for approval."))
+               .Then<If>(ifBuilder => ifBuilder
+                   .WithCondition(context => GetManagerApproval())
+                   .Then(then => then
+                       .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "Manager approved the leave request."))
+                       .Then<If>(innerIfBuilder => innerIfBuilder
+                           .WithCondition(context => GetHRApproval())
+                           .Then(then => then
+                               .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "HR approved the leave request."))
+                               .Then<WriteLine>(activity => activity.Set(x => x.Text, "Leave request is granted."))
+                               .Then((ActivityExecutionContext activity) => activity
+                                   .StartWith<WriteLine>(x => x.Set(y => y.Text, "Leave request approved"))
+                               )
+                               .Then(activity => activity
+                                   .StartWith<WriteLine>(x => x.Set(y => y.Text, "HR notified of approval"))
+                               )
+                           )
+                           .Else(elseBranch => elseBranch
+                               .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "HR rejected the leave request."))
+                               .Then(activity => activity
+                                   .StartWith<WriteLine>(x => x.Set(y => y.Text, "Leave request rejected"))
+                               )
+                           )
+                       )
+                   )
+                   .Else(elseBranch => elseBranch
+                       .StartWith<WriteLine>(activity => activity.Set(x => x.Text, "Manager rejected the leave request."))
+                       .Then(activity => activity
+                           .StartWith<WriteLine>(x => x.Set(y => y.Text, "Leave request rejected"))
+                       )
+                   )
+               );
     }
 }
